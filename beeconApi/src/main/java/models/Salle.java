bd5d49a794f103bec1366bac455bd4d1ebbae9d9
DonMarieLaure <marielaure.don@gmail.com>
2020-02-03 16:19:05 +0100

    Création du dao

--- beeconApi/src/main/java/models/Salle.java
+++ beeconApi/src/main/java/models/Salle.java
@@ -5,10 +5,31 @@
  */
 package models;
 
+import org.springframework.data.annotation.Id;
+
 /**
  *
  * @author marie
  */
 public class Salle {
+    @Id
+    private String NomSalle;
+    private double xDebut;
+    private double xFin;
+    private double yDebut;
+    private double yFin;
+    private Beacons MonBeacon;
+
+    public Salle (String NomSalle, double xDebut, double xFin, double yDebut, double yFin, Beacons MonBeacon){
+        this.NomSalle=NomSalle;
+        this.xDebut=xDebut;
+        this.xFin=xFin;
+        this.yDebut=yDebut;
+        this.yFin=yFin;
+        this.MonBeacon=MonBeacon;
+    }
     
+    public String getClef(){
+        return this.NomSalle;
+    }
 }
