be2aa2a8d2d4682e6c456082fcb550f756e2586b
DonMarieLaure <marielaure.don@gmail.com>
2020-02-04 09:29:56 +0100

    Modèle Salle et SalleDAO
    J'ai complété le modèle et le dao en suivant l'exemple Beacon.

--- beeconApi/src/main/java/isis/beecon/dao/SalleDAO.java
+++ beeconApi/src/main/java/isis/beecon/dao/SalleDAO.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package isis.beecon.dao;
+import isis.beecon.models.Salle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author marie
  */
-public class daoSalle {
-    
+@Repository
+public interface SalleDAO extends JpaRepository<Salle,Long>{
 }
--- beeconApi/src/main/java/isis/beecon/models/Salle.java
+++ beeconApi/src/main/java/isis/beecon/models/Salle.java
@@ -5,15 +5,18 @@
  */
 package isis.beecon.models;
 
+import javax.persistence.Entity;
 import org.springframework.data.annotation.Id;
 
 /**
  *
  * @author marie
  */
+@Entity(name = "Salle")
 public class Salle {
-    @Id
+    @javax.persistence.Id
     private String NomSalle;
+    
     private double xDebut;
     private double xFin;
     private double yDebut;
@@ -29,7 +32,54 @@ public class Salle {
         this.MonBeacon=MonBeacon;
     }
     
+    public Salle(){
+        
+    }
     public String getClef(){
         return this.NomSalle;
     }
+
+    public Beacons getMonBeacon() {
+        return MonBeacon;
+    }
+
+    public double getxDebut() {
+        return xDebut;
+    }
+
+    public double getxFin() {
+        return xFin;
+    }
+
+    public double getyDebut() {
+        return yDebut;
+    }
+
+    public double getyFin() {
+        return yFin;
+    }
+    
+    public void setNomSalle(String NomSalle) {
+        this.NomSalle = NomSalle;
+    }
+
+    public void setxDebut(double xDebut) {
+        this.xDebut = xDebut;
+    }
+
+    public void setxFin(double xFin) {
+        this.xFin = xFin;
+    }
+
+    public void setyDebut(double yDebut) {
+        this.yDebut = yDebut;
+    }
+
+    public void setyFin(double yFin) {
+        this.yFin = yFin;
+    }
+    
+    
 }
+
+
