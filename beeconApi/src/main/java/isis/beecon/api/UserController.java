6886e2f4529ff1d01581f60ebdfa65b61e3164da
Victoria <victoria.vichet@gmail.com>
2020-02-07 16:19:18 +0100

    modif UserController

--- beeconApi/src/main/java/isis/beecon/api/UserController.java
+++ beeconApi/src/main/java/isis/beecon/api/UserController.java
@@ -10,12 +10,14 @@ import isis.beecon.models.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author victoria
  */
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
     
     @Autowired
