3c3b5e507ad2594584f9e09befa2a0835fdcf411
Victoria <victoria.vichet@gmail.com>
2020-02-07 15:58:35 +0100

    ajout PositionDAO

---
+++ beeconApi/src/main/java/isis/beecon/dao/PositionDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isis.beecon.dao;
+
+import isis.beecon.models.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author victoria
+ */
+@Repository
+public interface PositionDAO extends JpaRepository<Position,Long>{   
+}
